5854983c2cda538d1556a5b64340d25f91b51150
Tulasi <tulsishuklag@gmail.com>
2026-06-24 21:21:54 +0530

    frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,9 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'   // ← Must be here
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
