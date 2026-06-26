96626137aaf8acca7af207d54c637c8858896075
Tulasi <tulsishuklag@gmail.com>
2026-06-26 13:59:44 +0530

    backend/server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => {
       console.log(`Server running on ${PORT}`);
